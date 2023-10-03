1d01cbe52e76056841ef8775446dd99e4fe3b8a3
Zonghero Jean Marc <jm.zonghero@gmail.com>
2023-10-03 15:23:29 +0200

    soucis pour récuperer pull

--- script.js
+++ script.js
@@ -103,18 +103,17 @@ let temps = 60;
 const timerElement = document.getElementById("timer");
 
 function diminuerTemps() {
-    document.querySelector("#imgChrono").className = "bouge";
+  document.querySelector("#imgChrono").className = "bouge";
 
-    let minutes = parseInt(temps / 60, 10);
-    let secondes = parseInt(temps % 60, 10);
+  let minutes = parseInt(temps / 60, 10);
+  let secondes = parseInt(temps % 60, 10);
 
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    secondes = secondes < 10 ? "0" + secondes : secondes;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  secondes = secondes < 10 ? "0" + secondes : secondes;
 
-    timerElement.innerText = `${minutes} : ${secondes}`;
-    temps = temps <= 0 ? 0 : temps - 1;
-  }
-  
-  setInterval(diminuerTemps, 1000);
- 
-  
+  timerElement.innerText = `${minutes} : ${secondes}`;
+  temps = temps <= 0 ? 0 : temps - 1;
+}
+
+setInterval(diminuerTemps, 1000);
+
